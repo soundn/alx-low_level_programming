e06a96ccb8899be3a350c4b5e8c13752e5adc069
soundn <soundn>
2022-09-08 07:00:00 -0700

    printf example

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry point
  *
- * return: Aways 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
