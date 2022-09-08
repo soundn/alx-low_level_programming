b8ff7ae2612291e2655f2ca9ecafa7f8a84aa776
soundn <soundn>
2022-09-08 12:46:15 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 /**
- *  *  * main - Entry point
- *   *   *
- *    *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 
