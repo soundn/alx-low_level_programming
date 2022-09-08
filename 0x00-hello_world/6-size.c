dcc4c3ff0dbf87a1292e5a233378949692d249ce
soundn <soundn>
2022-09-08 07:50:20 -0700

    print type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return Always (Successful)
+ */
+int main()
+{
+	int intType;
+	float floatType;
+	long int longintType;
+	char charType;
+	long long int longlongintType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+
+	return (0);
+}
