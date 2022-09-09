386fbceaf1843384c863ed50b55a9d6c56544989
soundn <soundn>
2022-09-09 11:19:38 -0700

    fix

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: kenneth nwankwo
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int no;
+
+	for (no = 'z'; no => 'a'; no--)
+		putchar((no);
+
+	putchar('\n');
+	return (0);
+}
