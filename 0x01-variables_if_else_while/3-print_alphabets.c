fd77ebca3a69f457e53986b5f6d813b3d82587ec
soundn <soundn>
2022-09-09 06:16:21 -0700

    first conditional statement in C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		putchar(ch);
+		for (ch = 'A'; ch <= 'Z'; ++ch)
+			putchar(ch);
+	putchar('\n');
+	return (0);
+}
