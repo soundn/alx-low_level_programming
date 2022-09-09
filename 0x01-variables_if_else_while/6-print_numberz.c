e5fa583b246191488b8367219a14e10426f07619
soundn <soundn>
2022-09-09 09:15:28 -0700

    fix 5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+/*
+ * File: 6-print_numberz.c
+ * Auth: kenneth nwankwo
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int no;
+
+	for (no = 0; no < 10; ++no)
+		putchar((no % 10) + '0');
+
+	putchar("\n");
+	return (0);
+}
