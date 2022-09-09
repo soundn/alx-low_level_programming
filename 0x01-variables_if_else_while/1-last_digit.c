ed195069cbd91b87ec1c1275378b75de55dc154a
soundn <soundn>
2022-09-09 05:24:13 -0700

    first conditional statement in C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n);
 	if (n == 0)
-		printf("Last digit of %d is %d and is zero\n", n);
+		printf("Last digit of %d is %d and is zero\n", n, n);
 	if (n < 6)
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, n);
 	return (0);
 }
