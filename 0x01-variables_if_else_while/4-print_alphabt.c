0ab930df99c6f4ea441a5e897f2394bfc8bd1103
soundn <soundn>
2022-09-09 06:37:06 -0700

    first conditional statement in C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+/*
+ * File: 4-print_alphabt.c
+ * 
+ * Auth: kenneth nwankwo
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
