39184970a303789476f8e5a2ed126e9e501696d7
soundn <soundn>
2022-09-09 09:09:11 -0700

    fix 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: kenneth nwankwo
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int no;
+
+	for (no = 0; no < 10; ++no)
+		printf("%d", no);
+	printf("\n")
+	return (0);
+}
