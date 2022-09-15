9c5847b64b0537d40553a1aaefb162a5044e7594
soundn <soundn>
2022-09-15 05:49:51 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,14 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * * print_number - a function that multiplies two integers
- * * @a: first input
- * * @b: second input
- * * Return: multiplication result of the 2 inputs
+ * * print_numbers - a function that multiplies two integers
+ * * Return: void
  */
 void print_numbers(void)
 {
 	int a;
 
-	for (a=0; a <= 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		_putchar('0' + a);
 	}
