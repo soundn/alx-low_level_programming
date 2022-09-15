28144bcca4a1b5589df54d18560450e887f2aaa1
soundn <soundn>
2022-09-15 03:51:43 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,17 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isupper - function checks for upper case.
+ * _isupper - checks if the letters are uppercase
+ * *  @c: input.
+ * *
+ * *  Return: 1 if c is uppercase
+ * 0 otherwise
  */
 int _isupper(int c)
 {
-	if (isupper(c))
+	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
 	else
-		return(0);
+		return (0);
 }
