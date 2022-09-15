b942e378f1ff36fa37e30bc25f795b3969dbbb93
soundn <soundn>
2022-09-15 03:24:04 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return(0);
+}
