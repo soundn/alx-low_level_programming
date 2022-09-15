58315ee5182644d2096068f6f4cd376eaad1c24e
soundn <soundn>
2022-09-15 05:12:22 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isdigit - function checks for 0 to 9.
+ * _isupper - checks if the letters are uppercase
+ * *  @c: input.
+ * *
+ * *  Return: 1 if c is uppercase
+ * 0 otherwise
+ */
+int mul(int a, int b)
+{
+	int result;
+	result = (a * b);
+	return (result);
+}
