25fb7ce8dde1d2a1b3f6ae3a386a793919a4a51c
soundn <soundn>
2022-09-19 07:13:08 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - function checks for upper case.
+ * _isupper - checks if the letters are uppercase
+ * *  @c: input.
+ * *
+ * *  Return: 1 if c is uppercase
+ * 0 otherwise
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+		
+}
