892744dfce785469a6ad07cc64cbeb4b44ab8d8c
soundn <soundn>
2022-09-21 15:34:44 -0700

    more pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /*
- * 0-strcat.c -show how to join two strings
+ * _strcat -show how to join two strings
  *
  */
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
-	return (0);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
