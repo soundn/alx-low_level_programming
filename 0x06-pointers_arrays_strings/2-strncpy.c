24675695d5f57501cd2811f6aa13057c8c75c509
soundn <soundn>
2022-09-21 16:27:19 -0700

    more strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - Concatenates the string pointed to by @src,
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ * @n: an integer
+ * Return: A pointer to the destination string @dest.
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
