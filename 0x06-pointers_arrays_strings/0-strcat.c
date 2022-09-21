eb1c079a286f66baf08764320d3015f60d29d789
soundn <soundn>
2022-09-21 15:21:37 -0700

    string and arrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ * 0-strcat.c -show how to join two strings
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ we continue with more pointers
