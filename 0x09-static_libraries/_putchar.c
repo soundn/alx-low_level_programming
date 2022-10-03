4d90e726a7e9b9ca1a5ef2d5672b763b57e6fd22
soundn <soundn>
2022-10-03 13:41:19 -0700

    fix

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,15 @@
+#include<unistd.h>
+
+/**
+ * _putchar - writes to the output
+ * @c: The character to print
+ * 
+ * Return: on success 1.
+ * on error, -1 is retuned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+}
