a78e0d509306e6c57d9d779b68df84ca8851a02c
soundn <soundn>
2022-10-10 08:42:49 -0700

    ”preprocessor”

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: dog.h
+ * Auth: kenneth sound
+ * Desc: Header file that defines a struct.
+ */
+
+struct dog {
+	char* name;
+	float age;
+	char* owner;
+}
+
+#endif
