5a63cac71aacafe3734d24252fd82a099dd700c2
soundn <soundn>
2022-10-10 09:05:13 -0700

    ”preprocessor”

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,9 +3,9 @@
 
 /*
  * File: 3-function_like_macro.h
- * Auth: kenneth sound
+ * Auth: ken
  * Desc: Header file that defines a function-like macro ABS(x)
- * that computes the absolute value of a number x.
+ *       that computes the absolute value of a number x.
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
