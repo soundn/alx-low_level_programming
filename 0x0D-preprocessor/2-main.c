42bc01fe15549c439741a62fc063590e28fe917c
soundn <soundn>
2022-10-10 08:06:08 -0700

    ”preprocessor”

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+/**
+ * main - function prints the file name
+ *
+ * Return: Always 0.
+ */
 
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
