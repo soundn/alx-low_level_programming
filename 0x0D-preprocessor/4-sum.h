bd81103c0af1c34b9cf5515ebccb490560112d9f
soundn <soundn>
2022-10-10 08:24:26 -0700

    ”preprocessor”

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Auth: kenneth sound
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
+ */
+
+#define SUM(x,y)  ((x) + (y))
+
+#endif
